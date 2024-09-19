b63fd48212309e063e0a651e0b78e4cfa5ba9606
Albrath <aloarter@gmail.com>
2024-09-19 18:42:12 +0200

    increased version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         applicationId = "com.p4r4d0x.hegemonytaxes"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
