6d2a5f828918c5570e39659776d0a192521cc950
Albrath <aloarter@gmail.com>
2025-01-26 12:27:13 +0100

    Fixed gh actions compilation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,12 +50,12 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-//            signingConfig = signingConfigs.getByName("release")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
     compileOptions {
